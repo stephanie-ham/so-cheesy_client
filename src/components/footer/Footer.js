b1c5d6321c25a2b980f12f6375126c54e7ce3c17
Stephanie Hamilton <hamiltons621@gmail.com>
2021-09-20 13:14:59 -0500

    added ScrollToTop.js for entire site and coded Footer.js. Still needs logo at bottom

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,45 @@
 import React from "react"
+import { Breadcrumbs } from "@material-ui/core";
+import { Link } from "react-router-dom"
 import "./Footer.css"
+import Pinterest from "../../images/pinterest-logo.png"
 
 export const Footer = () => {
   return (
-    <footer className="footer">This will be the footer!</footer>
+    <footer className="footer">
+      <section className="footer__top">
+        <div className="footer__logo">
+        <Link className="footer__item link" color="inherit" to="/"><h2>LOGO</h2></Link>
+        </div>
+        <div className="footer__links">
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link className="footer__item link" color="inherit" to="/create">
+              <strong>create</strong>
+            </Link>
+            <Link className="footer__item link" color="inherit" to="/ingredients/cheeses">
+              cheeses
+            </Link>
+            <Link className="footer__item link" color="inherit" to="/ingredients/meats">
+              meats
+            </Link>
+            <Link className="footer__item link" color="inherit" to="/ingredients/nuts">
+              nuts
+            </Link>
+            <Link className="footer__item link" color="inherit" to="/ingredients/fruits">
+              fruits
+            </Link>
+            <Link className="footer__item link" color="inherit" to="/ingredients/jams+spreads">
+              jams + spreads
+            </Link>
+          </Breadcrumbs>
+        </div>
+      </section>
+      <section className="footer__bottom">
+        <div className="footer__bottom--container">
+          <div className="fb__pinterest">Follow us on <a className="pinterest__link" href="https://www.pinterest.com/blockcharcuterie"><img className="fb__pinterest--logo" src={Pinterest} alt="" /> <b>Pinterest</b></a> for inspiration!</div>
+          <p className="fb__copyright">	&#169; 2021 <strong><em>Block.</em></strong> Charcuterie Maker &#160; | &#160; All Rights Reserved</p>
+        </div>
+      </section>
+    </footer>
   )
 }
